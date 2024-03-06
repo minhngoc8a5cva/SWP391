b81d94010ac6d1a48654ab6b4795b75c36533601
Bao Chau <ttbchau312@gmail.com>
2024-03-06 18:21:56 +0700

    File Product and ProductDAO trong model

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -6,7 +6,7 @@ package model;
 
 /**
  *
- * @author ADMIN
+ * @author chau
  */
 public class Product {
 
--- src/main/java/model/ProductDAO.java
+++ src/main/java/model/ProductDAO.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 /**
  *
- * @author ADMIN
+ * @author chau
  */
 public class ProductDAO {
 
